6d10347125aadd431351afe272823d990deb0ed7
Maciej Loj <maciej.loj@gmail.com>
2019-01-28 23:37:38 +0100

    added articles/views.py

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from articles.models import Article
+from django.http import HttpResponse
 
 # Create your views here.
+
 def article_list(request):
-    return render(request,'articles/articles.html')
+    artic = Article.objects.all().order_by('date')
+    return render(request,'articles/articles.html', {'articles': artic })
+
+
+def article_detail(request, slug):
+    #znajdz taki artykul ktorego slug = wyswietlonemu w url slugowi
+    article = Article.objects.get(slug=slug)
+    return render(request, 'articles/article_detail.html',{'article': article})
+    #return HttpResponse(slug)
