c90b8bdb33bc048034edaf5745f94303c7fcbd47
Maciej Loj <maciej.loj@gmail.com>
2019-01-31 02:27:41 +0100

    Changes

--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,12 @@
 
 from django.urls import path
-from firstapp.views import about, homepage
-from articles.views import article_list
+from django.conf.urls import url
+
+
+from articles import views
+
 
 urlpatterns = [
-    path('', article_list),
-    path('', homepage),
+
 ]
+
--- articles/views.py
+++ articles/views.py
@@ -1,16 +1,11 @@
 from django.shortcuts import render
-from articles.models import Article
-from django.http import HttpResponse
+
 
 # Create your views here.
 
-def article_list(request):
-    artic = Article.objects.all().order_by('date')
-    return render(request,'articles/articles.html', {'articles': artic })
+def portfolio(request):
+    return render(request,'articles/sesja.html')
 
-
-def article_detail(request, slug):
-    #znajdz taki artykul ktorego slug = wyswietlonemu w url slugowi
-    article = Article.objects.get(slug=slug)
-    return render(request, 'articles/article_detail.html',{'article': article})
-    #return HttpResponse(slug)
+def homepage(request):
+    return render(request,'homepage.html')
+    #return HttpResponse('homepage')
