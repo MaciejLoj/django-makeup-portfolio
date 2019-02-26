2ac3423a04371606e788b93f2cf7be17dd0882d8
Maciej Loj <maciej.loj@gmail.com>
2019-02-27 00:16:49 +0100

    portfolio update

--- articles/views.py
+++ articles/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 
 
-# Create your views here.
 
 def portfolio(request):
     return render(request,'articles/sesja.html')
 
+
 def homepage(request):
     return render(request,'homepage.html')
-    #return HttpResponse('homepage')
+
+
+def about(request):
+    return render(request,'articles/about.html')
--- djangoblog/urls.py
+++ djangoblog/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
-from django.urls import path, include
-from articles.views import portfolio, homepage
+from django.urls import path
+from articles.views import portfolio, homepage, about
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
@@ -10,10 +10,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage, name="homepage"),
     path('portfolio/', portfolio, name="portfolio"),
+    path('about-me/', about, name="about")
 ]
 
-#This will return the proper URL pattern for serving
-# static files to your already defined pattern list.
 urlpatterns += staticfiles_urlpatterns()
-# django is going to set up a url for us
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
