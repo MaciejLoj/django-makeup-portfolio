6e4e9750ed1a1aa3b7c3cd32ae50736cfe8c5ee5
Maciej Loj <maciej.loj@gmail.com>
2019-03-08 02:15:21 +0100

    Added secret location for credentials

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from .secret.secret import SECRET_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')le11-2c@_qs0a@c-d65gii*#gqk@()t%9&qte5)uhrls33^d*'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
